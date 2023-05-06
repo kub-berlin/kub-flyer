b70f4bab20c7edbc24f87dff7b0c44eee66bbae5
Tobias Bengfort <tobias.bengfort@posteo.de>
2023-05-06 12:59:26 +0200

    adapt extract_messages

--- extract_messages.sh
+++ extract_messages.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-grep -o '{{ [^}]* }}' templates/content.xml | sed 's/^{{ //;s/ }}$//;s/\(.*\)/"\1","\1"/' | sort | uniq > translations/de.csv
+grep -o '{{ [^}]* }}' templates/content.html | sed 's/^{{ //;s/ }}$//;s/\(.*\)/"\1","\1"/' | sort | uniq > translations/de.csv
